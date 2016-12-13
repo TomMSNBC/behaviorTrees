0bcfe6d93fa91399c7a5d0b0795e5ae312e11dbc
tomMMSNBC <tomwork667@gmail.com>
2016-12-13 15:35:12 -0800

    Btree for control rack

--- tests/js/controlRackNodes.js
+++ tests/js/controlRackNodes.js
@@ -14,91 +14,45 @@ controlRackNodes.actions = function(action) {
             // tick.blackboard.set('walking', 1);
             let name = tick.blackboard.get('name');
             console.log(name + ' Switching to play');
-
+            playButton.visible = true;
+            pauseButton.visible = false;
             return b3.SUCCESS;
         }
     });
-    action('openDoor', {
+    action('SwitchToPause', {
         tick: function(tick) {
-            tick.blackboard.set('walking', 0);
+            // tick.blackboard.set('walking', 1);
             let name = tick.blackboard.get('name');
-            console.log(name + ' is opening the door');
+            console.log(name + ' Switching to pause');
+            playButton.visible = false;
+            pauseButton.visible = true;
             return b3.SUCCESS;
         }
     });
-    action('smashDoor', {
+    action('SwitchToFullScreen', {
         tick: function(tick) {
-            tick.blackboard.set('walking', 0);
+            // tick.blackboard.set('walking', 1);
             let name = tick.blackboard.get('name');
-            console.log(name + ' is smashing the door');
-
+            console.log(name + ' Switching to full Screen');
             return b3.SUCCESS;
         }
     });
-    action('walkThroughDoor', {
+    action('TurnOnCC', {
         tick: function(tick) {
-            tick.blackboard.set('walking', 1);
+            // tick.blackboard.set('walking', 1);
             let name = tick.blackboard.get('name');
-            console.log(name + ' is walking through the door');
-
+            console.log(name + ' Turning on Closed Captions');
             return b3.SUCCESS;
         }
-    });
-    action('closeDoor', {
-        tick: function(tick) {
-            tick.blackboard.set('walking', 0);
-            let name = tick.blackboard.get('name');
-            console.log(name + ' is closing the door');
-
-            return b3.SUCCESS;
-        }
-    });
+    });       
+    
 }
 
 
 controlRackNodes.conditions = function(condition) {
-    condition('canIunlockTheDoor', {
-        tick: function(tick) {
-            var skill = tick.blackboard.get('lockpick-level');
-            let name = tick.blackboard.get('name');
-            if (!skill) {
-                skill = 0;
-                tick.blackboard.set('lockpick-level', skill);
-            }
-
-            if (skill > 5) {
-                console.log(name + ' picked the lock');
-                return b3.SUCCESS;
-            }
-
-            console.log(name + ' could not pick the lock');
-
-            skill += 1;
-            tick.blackboard.set('lockpick-level', skill);
-            return b3.FAILURE;
-        }
-    });
-    condition('IsDoorUnlocked', {
-        tick: function(tick) {
-            let name = tick.blackboard.get('name');
-            var locked = tick.blackboard.get('locked');
-            if (!locked) {
-                return b3.SUCCESS;
-            }
-            console.log(name + ' notices the door is locked');
-            return b3.FAILURE;
-        }
-    });
 
     condition('IsPauseClicked', {
         tick: function(tick) {
-            /*var point = agent.globalToLocal(stage.mouseX, stage.mouseY);
-                if (tick.target.hitTest(point.x, point.y)) {
-                return b3.SUCCESS;
-                } else {
-                return b3.FAILURE;
-            }
-            */
             pauseButton.b3 = b3;
             pauseButton.clicking = false;
             pauseButton.on("mousedown", function() {
@@ -121,5 +75,74 @@ controlRackNodes.conditions = function(condition) {
             
         }
     });
+    condition('IsPlayClicked', {
+        tick: function(tick) {
+            playButton.b3 = b3;
+            playButton.clicking = false;
+            playButton.on("mousedown", function() {
+                playButton.clicking = true;
+                if (!playButton.clicked) {
+                    playButton.clicked = true;
+                    console.log('play clicked');
+                }
+
+            });
+            if (playButton.clicked) {
+                playButton.clicked = false;
+                return b3.SUCCESS;
+            }
+            if (!playButton.clicking) {
+                playButton.clicked = false;
+            }
+            return b3.FAILURE;
+            
+        }
+    });    
+    condition('IsFullScreenClicked', {
+        tick: function(tick) {
+            fullScreen.b3 = b3;
+            fullScreen.clicking = false;
+            fullScreen.on("mousedown", function() {
+                fullScreen.clicking = true;
+                if (!fullScreen.clicked) {
+                    fullScreen.clicked = true;
+                    console.log('full clicked');
+                }
+
+            });
+            if (fullScreen.clicked) {
+                fullScreen.clicked = false;
+                return b3.SUCCESS;
+            }
+            if (!fullScreen.clicking) {
+                fullScreen.clicked = false;
+            }
+            return b3.FAILURE;
+            
+        }
+    }); 
+    condition('IsCCClicked', {
+        tick: function(tick) {
+            cc.b3 = b3;
+            cc.clicking = false;
+            cc.on("mousedown", function() {
+                cc.clicking = true;
+                if (!cc.clicked) {
+                    cc.clicked = true;
+                    console.log('cc clicked');
+                }
+
+            });
+            if (cc.clicked) {
+                cc.clicked = false;
+                return b3.SUCCESS;
+            }
+            if (!cc.clicking) {
+                cc.clicked = false;
+            }
+            return b3.FAILURE;
+            
+        }
+    });  
 }
 
