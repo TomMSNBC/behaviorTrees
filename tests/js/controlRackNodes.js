4179e82badf795f9b3ed34089dca137774820a80
tomMMSNBC <tomwork667@gmail.com>
2016-12-14 10:49:32 -0800

    added video

--- tests/js/controlRackNodes.js
+++ tests/js/controlRackNodes.js
@@ -16,6 +16,7 @@ controlRackNodes.actions = function(action) {
             console.log(name + ' Switching to play');
             playButton.visible = true;
             pauseButton.visible = false;
+            videoDom.play();
             return b3.SUCCESS;
         }
     });
@@ -26,6 +27,7 @@ controlRackNodes.actions = function(action) {
             console.log(name + ' Switching to pause');
             playButton.visible = false;
             pauseButton.visible = true;
+            videoDom.pause();
             return b3.SUCCESS;
         }
     });
@@ -44,7 +46,25 @@ controlRackNodes.actions = function(action) {
             console.log(name + ' Turning on Closed Captions');
             return b3.SUCCESS;
         }
-    });       
+    });
+    action('Mute', {
+        tick: function(tick) {
+            // tick.blackboard.set('walking', 1);
+            let name = tick.blackboard.get('name');
+            console.log(name + ' muting');
+            videoDom.muted = true;
+            return b3.SUCCESS;
+        }
+    }); 
+    action('UnMute', {
+        tick: function(tick) {
+            // tick.blackboard.set('walking', 1);
+            let name = tick.blackboard.get('name');
+            console.log(name + ' unmuting');
+            videoDom.muted = false;
+            return b3.SUCCESS;
+        }
+    });           
     
 }
 
@@ -144,5 +164,57 @@ controlRackNodes.conditions = function(condition) {
             
         }
     });  
+    condition('IsMuteClicked', {
+        tick: function(tick) {
+            muteoff.b3 = b3;
+            muteoff.clicking = false;
+            muteoff.on("mousedown", function() {
+                muteoff.clicking = true;
+                if (!muteoff.clicked) {
+                    muteoff.clicked = true;
+                    console.log('mute clicked');
+                    muteoff.visible = false;
+                    muteon.visible = true;
+                    videoDom.muted= true;
+                }
+
+            });
+            if (muteoff.clicked) {
+                muteoff.clicked = false;
+                return b3.SUCCESS;
+            }
+            if (!muteoff.clicking) {
+                muteoff.clicked = false;
+            }
+            return b3.FAILURE;
+            
+        }
+    });
+    condition('IsUnMuteClicked', {
+        tick: function(tick) {
+            muteon.b3 = b3;
+            muteon.clicking = false;
+            muteon.on("mousedown", function() {
+                muteon.clicking = true;
+                if (!muteoff.clicked) {
+                    muteon.clicked = true;
+                    console.log('unmute clicked');
+                    muteoff.visible = true;
+                    muteon.visible = false;
+                    videoDom.muted= false;
+                }
+
+            });
+            if (muteon.clicked) {
+                muteon.clicked = false;
+                return b3.SUCCESS;
+            }
+            if (!muteoff.clicking) {
+                muteoff.clicked = false;
+            }
+            return b3.FAILURE;
+            
+        }
+    });           
 }
 
--- tests/js/stage.js
+++ tests/js/stage.js
@@ -9,15 +9,34 @@ var bar;
 var fullScreen;
 var cc;
 
+var racky = 330;
 function start() {
   // CREATEJS
   canvas     = document.getElementById('canvas');
   stage      = new createjs.Stage(canvas);
   canvas.width = window.innerWidth;
 
+  drawVideo();
   drawControls();
   stage.update();
   loadTree();
+}
+
+function drawVideo() {
+//var videoDom = $('<video width="320" height="240" autoplay><source src="/tests/video/round.mp4" type="video/mp4"></video>').appendTo(document.body)[0];
+videoDom = document.createElement('video');
+videoDom.src = '/tests/video/round.mp4';
+videoDom.autoplay = false;
+var video2 = new createjs.Bitmap(videoDom);
+video2.x = 100;
+video2.y = 10;
+//video2.rotation = 10;
+video2.scaleX = 1.39;
+video2.scaleY = 1;
+video2.visible = true;
+
+//videoDom.pause();
+stage.addChild(video2);
 }
 
 function drawControls() {
@@ -31,32 +50,44 @@ function drawControls() {
   playButton = new createjs.Bitmap("/tests/img/play.png");
   playButton.visible = false;
   playButton.x = 100;
-  playButton.y = 200;
+  playButton.y = racky;
   stage.addChild(playButton);
 
   pauseButton = new createjs.Bitmap("/tests/img/pause.png");
   pauseButton.visible = true;
   pauseButton.x = 100;
-  pauseButton.y = 200;
+  pauseButton.y = racky;
   stage.addChild(pauseButton);
 
   bar = new createjs.Bitmap("/tests/img/bar.png");
   bar.visible = true;
   bar.x = 140;
-  bar.y = 200;
+  bar.y = racky;
   stage.addChild(bar);
 
   fullScreen = new createjs.Bitmap("/tests/img/fullscreen.png");
   fullScreen.visible = true;
   fullScreen.x = 640;
-  fullScreen.y = 200;
+  fullScreen.y = racky;
   stage.addChild(fullScreen);
 
   cc = new createjs.Bitmap("/tests/img/cc.png");
   cc.visible = true;
   cc.x = 682;
-  cc.y = 200;
+  cc.y = racky;
   stage.addChild(cc);
+
+  muteon = new createjs.Bitmap("/tests/img/muteon.png");
+  muteon.visible = false;
+  muteon.x = 725;
+  muteon.y = racky;
+  stage.addChild(muteon);
+
+  muteoff = new createjs.Bitmap("/tests/img/muteoff.png");
+  muteoff.visible = true;
+  muteoff.x = 725;
+  muteoff.y = racky;
+  stage.addChild(muteoff);
 
 }
 
